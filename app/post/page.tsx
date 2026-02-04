190de289026e8b5ff9f02c913f06275a7b2f2809
시원 <sw622@naver.com>
2026-02-04 17:13:21 +0900

    🧪 test: post CRUD 테스트 페이지 추가 - createPost, getPostById, updatePost, deletePost 테스트 UI - 개발/리뷰용 임시 페이지 (추후 제거 예정)

---
+++ app/post/page.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { deletePost, createPost, updatePost } from '@/data/actions/post';
+import { getPostById } from '@/data/functions/post';
+
+/**
+ * ‼️ DB 테스트용 페이지입니다.
+ * 삭제하고 구현해 주세요.
+ */
+export default function Post() {
+  // 테스트 postId : DB posts 테이블에서 id 확인 후 넣어서 조회/수정/삭제 테스트하시면 됩니다.
+  const postId = '5a38e2a7-705d-4045-8fef-336144cde5ad';
+
+  // [CREATE] 글 저장
+  const handleSave = async () => {
+    const res = await createPost({
+      topic: '글쓰기 주제',
+      title: '글쓰기 제목',
+      keywords: ['글', '작업'],
+      type: 'til',
+      content: '이렇게 글 쓰면 됩니다.',
+    });
+
+    console.log('저장 완료:', res);
+  };
+
+  // [READ] 글 조회
+  const handleGet = async () => {
+    const post = await getPostById(postId);
+
+    console.log('조회 결과:', post);
+  };
+
+  // [UPDATE] 글 수정
+  const handleUpdate = async () => {
+    const res = await updatePost(postId, {
+      topic: '수정된 주제',
+    });
+
+    console.log('수정 결과:', res);
+  };
+
+  // [DELETE] 글 삭제
+  const handleDelete = async () => {
+    const res = await deletePost(postId);
+
+    console.log('삭제 결과: ', res);
+  };
+
+  return (
+    <div>
+      <button onClick={handleSave}>글 저장</button>
+      <br />
+      <button onClick={handleGet}>글 조회</button>
+      <br />
+      <button onClick={handleUpdate}>글 수정</button>
+      <br />
+      <button onClick={handleDelete}>글 삭제</button>
+    </div>
+  );
+}
--- lib/auth.ts
+++ lib/auth.ts
@@ -5,14 +5,18 @@ import { createClient } from '@/lib/supabaseServer';
 export async function getAuthenticatedUser() {
   const supabase = await createClient();
 
-  const {
-    data: { user },
-    error,
-  } = await supabase.auth.getUser();
+  // 임시 user 데이터
+  const user = { id: '6226d361-ee1c-4342-9d74-ebb5ec532d40' };
 
-  if (!user || error) {
-    throw new Error('로그인이 필요합니다.');
-  }
+  // TODO 쿠키 사용으로 수정 예정
+  // const {
+  //   data: { user },
+  //   error,
+  // } = await supabase.auth.getUser();
+
+  // if (!user || error) {
+  //   throw new Error('로그인이 필요합니다.');
+  // }
 
   return { user, supabase };
 }
