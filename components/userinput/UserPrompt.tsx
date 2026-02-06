5ec80d133ff5dad3bfa62b96a0da1ea5ee6e93de
오서현 <bbibbi1118@naver.com>
2026-02-06 22:56:04 +0900

    💄 style: KeywordPrompt

--- components/userinput/UserPrompt.tsx
+++ components/userinput/UserPrompt.tsx
@@ -6,6 +6,7 @@ export default function UserPrompt() {
   return (
     <div className="px-4 py-7">
       <BlogPrompt />
+      <KeywordPrompt />
     </div>
   );
 }
@@ -17,6 +18,15 @@ export function BlogPrompt() {
     <div className="w-full">
       <p className="mb-1.5 text-sm leading-3.5 font-semibold text-black pc:mb-3 pc:text-lg pc:leading-4.5">블로그 내용</p>
       <textarea className="min-h-15 w-full resize-none rounded-sm border border-input-stroke px-2.5 py-2.5 text-sm leading-3.5 font-normal text-primary focus:ring-0 focus:outline-none pc:min-h-20 pc:px-3.5 pc:py-3 pc:text-base pc:leading-4" placeholder={`어떤 내용의 블로그 글을 작성하고 싶으신가요?\n예: 초보자를 위한 Next.js 시작하기 가이드`} value={value} onChange={e => setValue(e.target.value)} />
+    </div>
+  );
+}
+
+export function KeywordPrompt() {
+  return (
+    <div>
+      <p className="bg-amber-100">키워드</p>
+      <input type="text" placeholder="키워드를 입력하고 Enter를 누르세요." />
     </div>
   );
 }
