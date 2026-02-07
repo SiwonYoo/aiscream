f827e82c7ee6802fb8a21c71d1a309b803b22fc5
오서현 <bbibbi1118@naver.com>
2026-02-07 23:54:47 +0900

    💄 style: KeywordPrompt

--- components/userinput/UserPrompt.tsx
+++ components/userinput/UserPrompt.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import Image from 'next/image';
 
 export default function UserPrompt() {
   return (
@@ -17,7 +18,16 @@ export function BlogPrompt() {
   return (
     <div className="w-full">
       <p className="mb-1.5 text-sm leading-3.5 font-semibold text-black pc:mb-3 pc:text-lg pc:leading-4.5">블로그 내용</p>
-      <textarea className="min-h-15 w-full resize-none rounded-sm border border-input-stroke px-2.5 py-2.5 text-sm leading-3.5 font-normal text-primary focus:ring-0 focus:outline-none pc:min-h-20 pc:px-3.5 pc:py-3 pc:text-base pc:leading-4" placeholder={`어떤 내용의 블로그 글을 작성하고 싶으신가요?\n예: 초보자를 위한 Next.js 시작하기 가이드`} value={value} onChange={e => setValue(e.target.value)} />
+      <textarea
+        className="min-h-15 w-full resize-none rounded-sm border border-input-stroke px-2.5 py-2.5 text-sm leading-3.5 font-normal text-primary focus:ring-0 focus:outline-none pc:min-h-20 pc:px-3.5 pc:py-3 pc:text-base pc:leading-4"
+        placeholder={`어떤 내용의 블로그 글을 작성하고 싶으신가요?\n예: 초보자를 위한 Next.js 시작하기 가이드`}
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        onInput={e => {
+          e.currentTarget.style.height = 'auto';
+          e.currentTarget.style.height = `${e.currentTarget.scrollHeight}px`;
+        }}
+      />
     </div>
   );
 }
@@ -25,8 +35,14 @@ export function BlogPrompt() {
 export function KeywordPrompt() {
   return (
     <div>
-      <p className="bg-amber-100">키워드</p>
-      <input type="text" placeholder="키워드를 입력하고 Enter를 누르세요." />
+      <p className="mb-1.5 text-sm leading-3.5 font-semibold text-black">키워드</p>
+      <div className="flex items-center justify-between gap-3">
+        <textarea className="h-8.5 flex-1 border" placeholder="키워드를 입력하고 Enter를 누르세요." />
+        <button className="flex items-center justify-center rounded-sm border border-input-stroke p-2.5">
+          <Image src="/assets/images/ico-plus.svg" width={14} height={14} alt="생성하기 버튼 +" />
+        </button>
+      </div>
+      <span>Next.js</span>
     </div>
   );
 }
