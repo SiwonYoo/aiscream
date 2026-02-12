47b15028f21fa3035d7ca1743fa7ddc2158eadb5
YouVin <dbqls990909@naver.com>
2026-02-13 01:27:28 +0900

    💄style: 푸터 위치 수정

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -8,7 +8,7 @@ import { useAuth } from '@/app/hooks/useAuth';
 import { getMyPosts } from '@/data/functions/post';
 import type { Post } from '@/types/post';
 
-const MOCK_BLOG_TITLE = ['초보자를 위한 Next.js 시작하기 가이드', '프론트엔드 개발자 관점으로 바라보는 관심사의 분리와 좋은 폴더 구조 (feat. FSD)', 'Redux 어떻게 써야 잘 썼다고 소문이 날까?', 'Clean Architecture on Frontend'];
+// const MOCK_BLOG_TITLE = ['초보자를 위한 Next.js 시작하기 가이드', '프론트엔드 개발자 관점으로 바라보는 관심사의 분리와 좋은 폴더 구조 (feat. FSD)', 'Redux 어떻게 써야 잘 썼다고 소문이 날까?', 'Clean Architecture on Frontend'];
 export default function Sidebar() {
   const { logout } = useAuth();
   const { toggleSidebar, isSidebarOpen } = useUIStore();
@@ -29,7 +29,7 @@ export default function Sidebar() {
   }, []);
 
   return (
-    <aside id="sidebar" className={`z-10 flex w-70 flex-col self-stretch border-r border-base-stroke bg-muted text-sm transition-all duration-200 max-pc:absolute max-pc:inset-0 ${isSidebarOpen ? 'pc:w-75' : 'max-pc:w-0 pc:w-15.5'}`}>
+    <aside id="sidebar" className={`z-10 flex h-dvh w-70 flex-col self-stretch border-r border-base-stroke bg-muted text-sm transition-all duration-200 max-pc:absolute max-pc:inset-0 ${isSidebarOpen ? 'pc:w-75' : 'max-pc:w-0 pc:w-15.5'}`}>
       {/* (only-PC) 사이드바 헤더 */}
       <header className={`hidden h-15.5 border-b border-base-stroke px-4 py-4.5 pc:flex ${isSidebarOpen ? 'justify-between' : 'justify-center'}`}>
         {/* (only-PC) 로고 */}
@@ -51,9 +51,9 @@ export default function Sidebar() {
       </Link>
 
       {/* 블로그 목록 */}
-      <nav aria-label="블로그 목록" className={`mt-5 flex-1 space-y-1 px-4 pc:mx-3 pc:mt-8 pc:px-0`} aria-hidden={!isSidebarOpen}>
+      <nav aria-label="블로그 목록" className={`mt-5 flex-1 space-y-1 overflow-auto px-4 pc:mx-3 pc:mt-8 pc:px-0`} aria-hidden={!isSidebarOpen}>
         {!isSidebarOpen ? null : posts === null ? null : posts.length === 0 ? (
-          <div className="flex h-full items-center justify-center text-primary">아직 작성한 블로그가 없습니다.</div>
+          <div className="flex h-full justify-center text-primary">아직 작성한 블로그가 없습니다.</div>
         ) : (
           <ul>
             {posts.map((post, idx) => (
@@ -73,7 +73,7 @@ export default function Sidebar() {
       </nav>
 
       {/* 사이드바 푸터 */}
-      <footer className={`flex px-4 py-4.5 ${isSidebarOpen ? 'justify-between' : 'justify-center max-pc:hidden'}`}>
+      <footer className={`mt-auto flex px-4 py-4.5 ${isSidebarOpen ? 'justify-between' : 'justify-center max-pc:hidden'}`}>
         {/* 계정 정보 */}
         <span className={isSidebarOpen ? '' : 'hidden'}>email@gmail.com</span>
         {/* 로그아웃 버튼 */}
