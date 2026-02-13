14e63f0ffa6500da9d8d7d5478e394564bb29563
오서현 <bbibbi1118@naver.com>
2026-02-13 10:03:30 +0900

    ♻️ refactor: 화면 비율 재조정

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -3,7 +3,7 @@ import UserPrompt from '@/components/userinput/UserPrompt';
 
 export default function PostPage() {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex min-h-[95vh] flex-col pc:min-h-screen">
       <Base />
       <UserPrompt />
     </div>
