486cf0d01bd46155cb62f13707a8f708b74ec20b
onewayay <onewayay@naver.com>
2026-02-13 12:01:28 +0900

    💄style: not found의 로고 이미지 size 속성 수정

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center gap-10 px-4 pc:gap-15 pc:px-0">
       <div className="relative aspect-square w-25 pc:w-37.5">
-        <Image src="/assets/images/Aiscream_logo.svg" alt="AiScReam 로고" fill sizes="100px, (min-width: 768px) 150px" />
+        <Image src="/assets/images/Aiscream_logo.svg" alt="AiScReam 로고" fill sizes="(min-width: 768px) 150px, 100px" />
       </div>
       <div className="flex flex-col gap-4 text-center pc:gap-5">
         <h2 className="text-[22px] font-bold text-primary pc:text-4xl">요청하신 페이지가 존재하지 않습니다.</h2>
