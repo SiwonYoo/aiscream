a2f310f04beb786b7ab37ac50093fe2f757a9261
오서현 <bbibbi1118@naver.com>
2026-02-13 11:57:39 +0900

    💄 style: 드롭다운 수정

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -3,7 +3,7 @@ import UserPrompt from '@/components/userinput/UserPrompt';
 
 export default function PostPage() {
   return (
-    <div className="flex min-h-[95vh] flex-col pc:min-h-screen">
+    <div className="flex min-h-[93vh] flex-col pc:min-h-screen">
       <Base />
       <UserPrompt />
     </div>
