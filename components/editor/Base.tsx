e9b628f9d6a4457b8e4d0b3d39b64845c4fd915e
오서현 <bbibbi1118@naver.com>
2026-02-18 16:37:00 +0900

    ♻️ refactor: 오타수정

--- components/editor/Base.tsx
+++ components/editor/Base.tsx
@@ -7,7 +7,7 @@ export default function Base() {
     //  {/* 글 생성되기 전 상태 */}
     // {/* // 임시로 높이 min-h-screen 줬어요 */}
     <div className="base flex h-full w-full flex-1 flex-col items-center justify-center gap-6 px-4 pc:px-30">
-      <div className="pc:g-size-[44px] flex aspect-square w-15 items-center justify-center rounded-xl bg-keyword bg-[url('/assets/images/star-2x.png')] bg-size-[38px] bg-center bg-no-repeat pc:w-17.5"></div>
+      <div className="flex aspect-square w-15 items-center justify-center rounded-xl bg-keyword bg-[url('/assets/images/star-2x.png')] bg-size-[38px] bg-center bg-no-repeat pc:w-17.5 pc:bg-size-[44px]"></div>
       <h3 className="text-[28px] font-bold text-primary pc:text-[36px]">새로운 블로그 생성하기</h3>
       <p className="text-center text-sm text-secondary pc:text-base">
         아래에 블로그 글에 대한 설명과 키워드를 입력하면
