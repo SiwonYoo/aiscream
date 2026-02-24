0fd45a1680104419509a731b06b8be3a920fa5ef
오서현 <bbibbi1118@naver.com>
2026-02-24 16:44:12 +0900

    ♻️ refactor: z-1에서 z-[1]로 수정

--- components/common/Loading.tsx
+++ components/common/Loading.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 export default function Loading({ autoplay = true, loop = true }: Props) {
   return (
-    <div className="absolute top-0 right-0 bottom-0 left-0 z-1 flex flex-col items-center justify-center gap-6 bg-[rgba(0,0,0,0.5)] dark:bg-[rgba(255,255,255,0.40)]">
+    <div className="absolute top-0 right-0 bottom-0 left-0 z-[1] flex flex-col items-center justify-center gap-6 bg-[rgba(0,0,0,0.5)] dark:bg-[rgba(255,255,255,0.40)]">
       <div className="aspect-square w-20 overflow-hidden rounded-xl pc:w-30 pc:rounded-2xl">
         <Lottie animationData={icecream} autoplay={autoplay} loop={loop} />
       </div>
