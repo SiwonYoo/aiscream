efe0b3e671c504939c24cabc05378ff01c9f6d21
오서현 <bbibbi1118@naver.com>
2026-02-24 16:42:39 +0900

    ♻️ refactor: SVG filter ID 비고유 — 다중 인스턴스 렌더 시 충돌

--- components/common/DropIcon.tsx
+++ components/common/DropIcon.tsx
@@ -1,7 +1,10 @@
+import { useId } from 'react';
+
 export default function DropIcon({ className }: { className?: string }) {
+  const filterId = useId();
   return (
     <svg width="60" height="29" viewBox="0 0 60 29" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
-      <g>
+      <g filter={`url(#${filterId})`}>
         <path d="M6 4L54 4L45.7935 20.8L14.2065 20.8L6 4Z" fill="var(--color-muted)" />
         <path
           d="M35.2414 9.56046C35.1912 9.50968 35.1316 9.4694 35.066 9.44191C35.0004 9.41442 34.9301 9.40027 34.8591 9.40027C34.7881 9.40027 34.7178 9.41442 34.6522 9.44191C34.5866 9.4694 34.527 9.50968 34.4768 9.56046L29.9999 14.083L25.5229 9.56046C25.4215 9.45808 25.284 9.40055 25.1406 9.40055C24.9972 9.40055 24.8597 9.45808 24.7583 9.56046C24.6569 9.66285 24.6 9.80172 24.6 9.94652C24.6 10.0913 24.6569 10.2302 24.7583 10.3326L29.6176 15.2401C29.6677 15.2909 29.7273 15.3311 29.7929 15.3586C29.8585 15.3861 29.9288 15.4003 29.9999 15.4003C30.0709 15.4003 30.1412 15.3861 30.2068 15.3586C30.2724 15.3311 30.332 15.2909 30.3821 15.2401L35.2414 10.3326C35.2917 10.2819 35.3315 10.2218 35.3588 10.1555C35.386 10.0893 35.4 10.0182 35.4 9.94652C35.4 9.8748 35.386 9.80378 35.3588 9.73753C35.3315 9.67129 35.2917 9.61111 35.2414 9.56046Z"
@@ -9,7 +12,7 @@ export default function DropIcon({ className }: { className?: string }) {
         />
       </g>
       <defs>
-        <filter id="filter0_d_1_77" x="0" y="0" width="60" height="28.8" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+        <filter id="{filterId}" x="0" y="0" width="60" height="28.8" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
           <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
           <feOffset dy="2" />
